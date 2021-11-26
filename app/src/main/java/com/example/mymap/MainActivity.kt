6cb45479767dee0f96dd0e4f7798beb5fa51f264
nthahaianh <ntha.haianh@gmail.com>
2021-11-26 07:39:18 +0700

    commit-update

--- app/src/main/java/com/example/mymap/MainActivity.kt
+++ app/src/main/java/com/example/mymap/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.mymap
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ContentInfoCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermission()
+        main_btnReload.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkConnect(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val info = connectivityManager.activeNetworkInfo
+        return if (info == null || !info.isConnected || !info.isAvailable) {
+            Toast.makeText(baseContext, "No internet connection", Toast.LENGTH_SHORT).show()
+            false
+        } else {
+            true
+        }
+        return false
+    }
+
+    private fun checkPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
+            return
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && checkConnect()) {
+                    var intent = Intent(this,MapsActivity::class.java)
+                    startActivity(intent)
+                    finishAffinity()
+                }else{
+                    main_btnReload.visibility = View.VISIBLE
+                    main_tvNoti.visibility = View.VISIBLE
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
--- app/src/main/java/com/example/mymap/MapsActivity.kt
+++ app/src/main/java/com/example/mymap/MapsActivity.kt
@@ -1,43 +1,181 @@
 package com.example.mymap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import java.util.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener,GoogleMap.OnMapLongClickListener {
 
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
+
+    val hanoi = LatLng(21.029634028797496, 105.85289362818004)
     private lateinit var mMap: GoogleMap
+    private var currentLocation: Location? = null
+    private var destinationLocation: Location? = null
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    var currentMarker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+//        setUpMap()
+        map_ivNavigation.setOnClickListener {
+            if(map_tvAddress==null || map_tvAddress.text.toString() == ""){
+                Toast.makeText(baseContext,"Select a place",Toast.LENGTH_SHORT).show()
+            }else{
+                map_llSearch.visibility = View.GONE
+                map_llAddress.visibility = View.VISIBLE
+            }
+        }
+        map_btnSubmit.setOnClickListener {
+            Toast.makeText(baseContext, "Navigation", Toast.LENGTH_SHORT).show()
+        }
+        map_ivSearch.setOnClickListener {
+            map_llAddress.visibility = View.GONE
+            map_llSearch.visibility = View.VISIBLE
+        }
+        map_btnSearch.setOnClickListener {
+            searchLocation(map_etSearch.text.toString())
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.setOnMapLongClickListener(this)
+        mMap.setOnMapClickListener(this)
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.M){
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_REQUEST_CODE
+                )
+                return
+            }
+        }
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isMapToolbarEnabled = false
+//        mMap.addMarker(MarkerOptions().position(hanoi).title("Marker in Hanoi"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(hanoi))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(hanoi,12f))
+//        val latLng = LatLng(currentLocation?.latitude!!,currentLocation?.longitude!!)
+//        addMarkerOnMap(latLng)
+
+        mMap.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
+            override fun onMarkerDrag(p0: Marker) {
+            }
+
+            override fun onMarkerDragEnd(p0: Marker) {
+                if (currentMarker != null) {
+                    currentMarker?.remove()
+                }
+                val newPosition = LatLng(p0.position.latitude, p0.position.longitude)
+                addMarkerOnMap(newPosition)
+            }
+
+            override fun onMarkerDragStart(p0: Marker) {
+            }
+
+        })
+    }
+
+    private fun addMarkerOnMap(currentLatLong: LatLng) {
+        var address = getAdd(currentLatLong.latitude, currentLatLong.longitude)
+        val markerOptions = MarkerOptions()
+            .position(currentLatLong)
+            .draggable(true)
+            .title("$currentLatLong")
+            .snippet(address)
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+            .alpha(0.7f)
+        map_tvAddress.setText(address)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLong))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 15f))
+        currentMarker = mMap.addMarker(markerOptions)
+//        currentMarker?.showInfoWindow()
+    }
+
+    private fun getAdd(latitude: Double, longitude: Double): String? {
+        val geoCoder = Geocoder(this, Locale.getDefault())
+        try {
+            val address = geoCoder.getFromLocation(latitude, longitude, 1)
+            return address[0].getAddressLine(0)
+        } catch (e: Exception) {
+            e.stackTrace
+        }
+        return "---"
+    }
+
+    private fun searchLocation(searchString: String) {
+        var addressList:List<Address>?=null
+        if (searchString==""){
+            Toast.makeText(baseContext, "Empty", Toast.LENGTH_SHORT).show()
+        }else{
+            val geoCoder = Geocoder(this)
+            try {
+                addressList = geoCoder.getFromLocationName(searchString,1)
+            } catch (e: Exception) {
+                e.stackTrace
+            }
+            if (addressList?.size!! >0){
+                val address = addressList?.get(0)
+                val searchPosition = address?.let { LatLng(it.latitude,address.longitude) }
+                if (searchPosition != null) {
+                    if (currentMarker != null) {
+                        currentMarker?.remove()
+                    }
+                    addMarkerOnMap(searchPosition)
+                    map_llSearch.visibility = View.GONE
+                    map_llAddress.visibility = View.VISIBLE
+                }
+            }else{
+                Toast.makeText(baseContext, "No result", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onMapLongClick(point: LatLng) {
+        if (currentMarker != null) {
+            currentMarker?.remove()
+        }
+        val newPosition = LatLng(point.latitude, point.longitude)
+        addMarkerOnMap(newPosition)
+    }
+
+    override fun onMapClick(p0: LatLng) {
+        map_llSearch.visibility = View.GONE
+        map_llAddress.visibility = View.GONE
     }
 }
